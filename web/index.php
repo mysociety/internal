196dfae6450b02de270af093ae21085bbc9e91c5
francis <francis>
2006-08-18 12:28:48 +0000

    No space in default nickname

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ include "../wordpress/wp-content/themes/mysociety/header.php";
 <table border="0" cellpadding="5" cellspacing="0">
 <tr><td colspan="2" align="center" bgcolor="#c0c0dd"><b>mySociety Chat Login</b></td></tr>
 <tr><td align="right" bgcolor="#f1f1f1">Nickname</td><td align="left"
-bgcolor="#f1f1f1"><input type="text" name="Nickname" value="Passer by"></td></tr>
+bgcolor="#f1f1f1"><input type="text" name="Nickname" value="Passerby"></td></tr>
 <tr><td align="left" bgcolor="#d9d9d9">
 </td><td align="right" bgcolor="#d9d9d9">
 <input type="submit" value="Login" style="background-color: #d9d9d9">
