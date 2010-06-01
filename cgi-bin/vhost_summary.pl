e172afabb1305264c6559092b23d7f6ef4bb383c
dave <dave>
2010-06-01 17:11:23 +0000

    correct script (wrong version checked in)

--- cgi-bin/vhost_summary.pl
+++ cgi-bin/vhost_summary.pl
@@ -10,78 +10,87 @@
 
 my $contentType = "html"; # set "html" for standalone page, or "plain" for inclusion in TWiki
 
-my $vhostsFilename = "/data/servers/vhosts.pl";
-
-$vhostsFilename = "vhosts.pl" unless -e $vhostsFilename; # uses a local copy if can't find the real one (useful for offline testing!)
+my @vhostsFilenames = ("/data/servers/vhosts.pl", "./vhosts.pl"); # try in this order: useful for testing
 
 my $isInList = 0;
-my @report;
-my @problems;
 
 print "Content-type: text/$contentType\n\n";
 
-unless ($return = do $vhostsFilename) {
-    push @problems, "Couldn't parse $vhostsFilename: $@\n" if $@;
-    push @problems, "Couldn't do $vhostsFilename: $!\n" unless defined $return;
-    push @problems, "Couldn't run $vhostsFilename\n" unless $return;
-    if (@problems){
-        say "---++ Problem (sorry, $0 failed)\n";
-        say "   * $_" foreach @problems;
+my $vhostsFilename = shift @vhostsFilenames;
+while ($vhostsFilename){
+    if ($return = do $vhostsFilename) {
+        last;
+    } else {
+        my @problems;
+        push @problems, "Couldn't parse =$vhostsFilename= &mdash; $@\n" if $@;
+        push @problems, "Couldn't do =$vhostsFilename= &mdash; $!\n" unless defined $return;
+        push @problems, "Couldn't run =$vhostsFilename=\n" unless $return;
+        if (@problems){
+            say("---++ Problem (sorry, $0 failed)\n");
+            say("   * $_") foreach @problems;
+        }
+        if ($vhostsFilename = shift @vhostsFilenames){
+            say("   * <span style='background:#ff0000;color:#ffffff;padding:4px;'>now trying $vhostsFilename instead...</span>");
+        }
     }
 }
 
-if (not @problems){
-  my %vh = %$vhosts if $vhosts; # $vhosts is declared in vhosts.pl
-  my %vhostsByServer; # to contain: e.g., 'balti' => ['dave.fixmystreet.com', 'gut.dave.thingummy']
-  my ($totalVhosts, %vhostsByName);
-  foreach my $vhost (sort keys %vh) {
-      my $hRef = $vh{$vhost};
-      my %vhostData = %$hRef;
-      my $aRef = $vhostData{"servers"};
-      my @servers = @$aRef if ref $aRef eq 'ARRAY';
-      $vhostsByName{$vhost} ||= 0;
-      foreach my $server (@servers){ # because a vhost may be deployed on more than one server
-          $aRef = $vhostsByServer{$server};
-          my @vhostsOnThisServer = $aRef? @$aRef : ();
-          push @vhostsOnThisServer, $vhost;        
-          $vhostsByServer{$server}=\@vhostsOnThisServer;
-          $vhostsByName{$vhost}++;
-      }
-  }
+my %vh = %$vhosts if $vhosts; # $vhosts is declared in vhosts.pl
 
-  my $totalServers = 0;
+my %vhostsByServer; # to contain: e.g., 'balti' => ['dave.fixmystreet.com', 'gut.dave.thingummy']
 
-  foreach my $server (sort keys %vhostsByServer){ 
-      $totalServers++;
-      my $aRef = $vhostsByServer{$server};
-      my @vhostsOnThisServer = @$aRef if $aRef;
-      my $cVhosts = @vhostsOnThisServer;
-      say("---++ $server");
-      if ($cVhosts){
-          say(($cVhosts-1?"$cVhosts vhosts are":"Only one vhost is") . " configured on $server:");
-          foreach (sort @vhostsOnThisServer){
-            my $b = /^(www|secure|news|intranet|lists)\./? '*':'';
-            say("   * $b$_$b");
-            $totalVhosts++;
-          } 
-      } else {
-          say("No vhosts running on this server.")
-      }
-  }
-  my (@homeless, $totalUniqueVhosts);
-  foreach (sort keys %vhostsByName){
-      if ($vhostsByName{$_}){
-          $totalUniqueVhosts++
-      } else {
-          push @homeless, $_
-      }
-   }
-  my $selfLink = qq{ | <a href="$0" target="_top">click for full page</a>};
-  say("=$vhostsFilename parsed at " . gmtime() . "=" . $selfLink); 
-  presay("Vhosts declared but not on any server: " . join(", ", @homeless)) if @homeless;
-  presay("Total: $totalVhosts vhosts ($totalUniqueVhosts unique) configured across $totalServers servers. $selfLink");
-  presay("---+ vhosts.pl summary");
+my ($totalVhosts, %vhostsByName);
+
+foreach my $vhost (sort keys %vh) {
+    my $hRef = $vh{$vhost};
+    my %vhostData = %$hRef;
+    my $aRef = $vhostData{"servers"};
+    my @servers = @$aRef if ref $aRef eq 'ARRAY';
+    $vhostsByName{$vhost} ||= 0;
+    foreach my $server (@servers){ # because a vhost may be deployed on more than one server
+        $aRef = $vhostsByServer{$server};
+        my @vhostsOnThisServer = $aRef? @$aRef : ();
+        push @vhostsOnThisServer, $vhost;        
+        $vhostsByServer{$server}=\@vhostsOnThisServer;
+        $vhostsByName{$vhost}++;
+    }
 }
+
+my $totalServers = 0;
+my @report;
+
+foreach my $server (sort keys %vhostsByServer){ 
+    $totalServers++;
+    my $aRef = $vhostsByServer{$server};
+    my @vhostsOnThisServer = @$aRef if $aRef;
+    my $cVhosts = @vhostsOnThisServer;
+    say("---++ $server");
+    if ($cVhosts){
+        say(($cVhosts-1?"$cVhosts vhosts are":"Only one vhost is") . " configured on $server:");
+        foreach (sort @vhostsOnThisServer){
+          my $b = /^(www|secure|news|intranet|lists)\./? '*':'';
+          say("   * $b$_$b");
+          $totalVhosts++;
+        } 
+    } else {
+        say("No vhosts running on this server.")
+    }
+}
+my @homeless;
+my $totalUniqueVhosts = 0;
+
+foreach (sort keys %vhostsByName){
+    if ($vhostsByName{$_}){
+        $totalUniqueVhosts++
+    } else {
+        push @homeless, $_
+    }
+}
+my $selfLink = qq{ | <a href="$0" target="_top">click for full page</a>};
+say("=$vhostsFilename parsed at " . gmtime() . "=" . $selfLink) if $vhostsFilename; 
+presay("Vhosts declared but not on any server: " . join(", ", @homeless)) if @homeless;
+presay("Total: $totalVhosts vhosts ($totalUniqueVhosts unique) configured across $totalServers servers. $selfLink");
+presay("---+ vhosts.pl summary");
 
 my $report = join "\n", @report;
 my $html = do {local $/;<DATA>};
@@ -94,7 +103,7 @@ sub say{push @report, twiki2html(shift)}
 sub twiki2html{
     my $line = shift;
     return $line if $contentType ne "html";
-    $line =~ s/=((\/|\w)[^=]*)=(?=\s)/<code>$1<\/code>/g;
+    $line =~ s/=((\/|\.|\w)[^=]*)=(?=\s)/<code>$1<\/code>/g;
     $line =~ s/\*(\S.*?)\*/<b>$1<\/b>/g;
     if ($line=~/^   \*(.*)/){
         $line = "<li> $1 </li>";
