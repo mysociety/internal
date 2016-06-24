07a6eecd983a2033604703be7d5de92fed0764cd
Louise Crow <louise.crow@gmail.com>
2016-06-24 10:59:25 +0100

    Remove sitestats.

--- sitestats/pylib/sitestats/newsletters/sources/__init__.py
+++
@@ -1,4 +0,0 @@
-from sitestats.newsletters.sources.google import *
-from sitestats.newsletters.sources.piwik import *
-from sitestats.newsletters.sources.twfy_api import *
-from sitestats.newsletters.sources.fms_api import *
